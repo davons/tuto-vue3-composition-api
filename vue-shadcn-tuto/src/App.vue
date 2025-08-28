d524d17e1162df2794389a992d9a0cb2accb1997
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-08-28 14:04:05 +0200

    Form validation

--- vue-shadcn-tuto/src/App.vue
+++ vue-shadcn-tuto/src/App.vue
@@ -2,8 +2,9 @@
 import LoginForm from './components/LoginForm.vue'
 import AlertPlayground from './components/6/exemple/AlertPlayground.vue'
 import AlertDialogPlayground from './components/6/exemple/AlertDialogPlayground.vue'
+import DialogPlayground from './components/6/exemple/DialogPlayground.vue'
 </script>
 
 <template>
-  <AlertDialogPlayground />
+  <LoginForm class="m-5" />
 </template>
