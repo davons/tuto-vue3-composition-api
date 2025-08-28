fe6bbdec7d4639b2175e1b70c71bb599dd1133fa
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-08-28 22:17:46 +0200

    Toast

--- vue-shadcn-tuto/src/App.vue
+++ vue-shadcn-tuto/src/App.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
-import LoginForm from './components/LoginForm.vue'
-import AlertPlayground from './components/6/exemple/AlertPlayground.vue'
-import AlertDialogPlayground from './components/6/exemple/AlertDialogPlayground.vue'
-import DialogPlayground from './components/6/exemple/DialogPlayground.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Toaster } from '@/components/ui/sonner'
+import { toast } from 'vue-sonner'
+import { Button } from '@/components/ui/button'
+import 'vue-sonner/style.css'
+
+const handleToast = () => {
+  toast('Event has been created', {
+    description: 'Sunday, December 03, 2023 at 9:00 AM',
+    action: {
+      label: 'Undo',
+      onClick: () => console.log('Undo'),
+    },
+  })
+}
 </script>
 
 <template>
-  <LoginForm class="m-5" />
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <Button variant="outline" @click="handleToast"> Add to calendar </Button>
+    </main>
+  </SidebarProvider>
+  <Toaster />
 </template>
