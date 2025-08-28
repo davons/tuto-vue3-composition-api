95719349b93e1d0330da449c06a7730b1067fdad
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-08-28 13:01:52 +0200

    alert dialog

--- vue-shadcn-tuto/src/App.vue
+++ vue-shadcn-tuto/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import LoginForm from './components/LoginForm.vue'
+import AlertPlayground from './components/6/exemple/AlertPlayground.vue'
+import AlertDialogPlayground from './components/6/exemple/AlertDialogPlayground.vue'
 </script>
 
 <template>
-  <LoginForm class="m-5" />
+  <AlertDialogPlayground />
 </template>
---
+++ vue-shadcn-tuto/src/components/6/exemple/AlertDialogPlayground.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+const handleCancel = () => {
+  console.log('cancel')
+}
+
+const handleContinue = () => {
+  console.log('continue')
+}
+
+const open = ref(false)
+</script>
+
+<template>
+  <AlertDialog v-model:open="open">
+    <AlertDialogTrigger>Open</AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your account and remove your
+          data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel @click="handleCancel">Cancel</AlertDialogCancel>
+        <AlertDialogAction @click="handleContinue">Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
