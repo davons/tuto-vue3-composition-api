c54ce9ee4da4da261541c3cfe75d336475b0e0a1
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-09-01 12:40:35 +0200

    Option API PINIA

--- vuejs-pinia/src/components/TheHeader.vue
+++ vuejs-pinia/src/components/TheHeader.vue
@@ -1,6 +1,16 @@
-<script setup>
+<script>
 // imports
 import CartWidget from './CartWidget.vue';
+import { useAuthUserStore } from '../stores/AuthUserStore';
+import { mapState } from 'pinia';
+
+export default {
+  components: {CartWidget},
+  computed: {
+    ...mapState(useAuthUserStore, ["username"])
+  }
+}
+
 </script>
 
 <template>
@@ -9,6 +19,10 @@ import CartWidget from './CartWidget.vue';
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div>
+      <span class="mr-5">{{ username }}</span>
+      <CartWidget class="inline-block"/>
+    </div>
+
   </header>
 </template>
