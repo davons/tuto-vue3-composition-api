5dd3b393bb5d6d694356a99b249726f9ae391f6a
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-09-07 11:32:50 +0200

    templateRefs

--- vuejs-modal-project/src/App.vue
+++ vuejs-modal-project/src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+
+const title = ref('My first new App')
+const input = useTemplateRef('my-input')
+
+const handleClick = () => {
+  const elem = input.value as HTMLElement
+  elem.focus()
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="my-input" />
+  <button @click="handleClick">Ref</button>
 </template>
 
 <style scoped></style>
